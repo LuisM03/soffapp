308b306b8e1229e7b57c8d305d166b3373360441
DIDIER321 <decastaneda949@misena.educo>
2023-06-13 11:21:16 -0500

    crud Proveedores

---
+++ soffapp/Controllers/ProveedorsController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using soffapp.Models;
+
+namespace soffapp.Controllers
+{
+    public class ProveedorsController : Controller
+    {
+        private readonly SoffDatabaseContext _context;
+
+        public ProveedorsController(SoffDatabaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Proveedors
+        public async Task<IActionResult> Index()
+        {
+            var soffDatabaseContext = _context.Proveedors.Include(p => p.IdInsumoNavigation);
+            return View(await soffDatabaseContext.ToListAsync());
+        }
+
+        // GET: Proveedors/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null || _context.Proveedors == null)
+            {
+                return NotFound();
+            }
+
+            var proveedor = await _context.Proveedors
+                .Include(p => p.IdInsumoNavigation)
+                .FirstOrDefaultAsync(m => m.IdProveedor == id);
+            if (proveedor == null)
+            {
+                return NotFound();
+            }
+
+            return View(proveedor);
+        }
+
+        // GET: Proveedors/Create
+        public IActionResult Create()
+        {
+            ViewData["IdInsumo"] = new SelectList(_context.Insumos, "IdInsumo", "IdInsumo");
+            return View();
+        }
+
+        // POST: Proveedors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdProveedor,IdInsumo,Nombre,Empresa,Direccion,FechaRegistro,Correo,Telefono,Ciudad,Estado")] Proveedor proveedor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(proveedor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdInsumo"] = new SelectList(_context.Insumos, "IdInsumo", "IdInsumo", proveedor.IdInsumo);
+            return View(proveedor);
+        }
+
+        // GET: Proveedors/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null || _context.Proveedors == null)
+            {
+                return NotFound();
+            }
+
+            var proveedor = await _context.Proveedors.FindAsync(id);
+            if (proveedor == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdInsumo"] = new SelectList(_context.Insumos, "IdInsumo", "IdInsumo", proveedor.IdInsumo);
+            return View(proveedor);
+        }
+
+        // POST: Proveedors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("IdProveedor,IdInsumo,Nombre,Empresa,Direccion,FechaRegistro,Correo,Telefono,Ciudad,Estado")] Proveedor proveedor)
+        {
+            if (id != proveedor.IdProveedor)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(proveedor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProveedorExists(proveedor.IdProveedor))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdInsumo"] = new SelectList(_context.Insumos, "IdInsumo", "IdInsumo", proveedor.IdInsumo);
+            return View(proveedor);
+        }
+
+        // GET: Proveedors/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null || _context.Proveedors == null)
+            {
+                return NotFound();
+            }
+
+            var proveedor = await _context.Proveedors
+                .Include(p => p.IdInsumoNavigation)
+                .FirstOrDefaultAsync(m => m.IdProveedor == id);
+            if (proveedor == null)
+            {
+                return NotFound();
+            }
+
+            return View(proveedor);
+        }
+
+        // POST: Proveedors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            if (_context.Proveedors == null)
+            {
+                return Problem("Entity set 'SoffDatabaseContext.Proveedors'  is null.");
+            }
+            var proveedor = await _context.Proveedors.FindAsync(id);
+            if (proveedor != null)
+            {
+                _context.Proveedors.Remove(proveedor);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProveedorExists(long id)
+        {
+          return (_context.Proveedors?.Any(e => e.IdProveedor == id)).GetValueOrDefault();
+        }
+    }
+}
